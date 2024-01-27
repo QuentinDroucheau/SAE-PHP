5fd7785ace21f395eb416aa6619d19a881a1145b
QuentinDroucheau <quentin.droucheau@etu.univ-orleans.fr>
2024-01-27 12:12:40 +0100

    renommage dossier

--- db/sqlite.php
+++ db/sqlite.php
@@ -1,8 +1,8 @@
 <?php 
 
 try{
-    $sql = file_get_contents("bd/table.sql");
-    $db = new \PDO("sqlite:bd/database.sqlite3");
+    $sql = file_get_contents("db/table.sql");
+    $db = new \PDO("sqlite:db/database.sqlite3");
     $db->exec($sql);
     $db = null;
 }catch(\Exception $e){
