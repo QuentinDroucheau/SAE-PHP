6fb66fbb80d4df5cc23f1d193e9621ff4610e16c
QuentinDroucheau <quentin.droucheau@etu.univ-orleans.fr>
2024-01-27 22:25:06 +0100

    modification méthode render

--- classes/controller/Controller.php
+++ classes/controller/Controller.php
@@ -12,25 +12,25 @@ abstract class Controller{
 
     /**
      * affiche une vue
-     * @param string $view
+     * @param string $view nom de la vue
      * @param array $variale
      */
     public function render(string $view, array $variable = []): void{
         extract($variable);
-        require "view/".$view;
+        require "view/".$view.".php";
     }
 
     /**
      * retourne le contenu d'une vue 
      * utile pour imbriquer des vues entre elles
-     * @param string $view
+     * @param string $view nom de la vue
      * @param array $variable
      * @return string
      */
     public function get(string $view, array $variable = []): string{
         ob_start();
         extract($variable);
-        require "view/".$view;
+        require "view/".$view.".php";
         $content = ob_get_clean();
         return $content;
     }
--- classes/controller/ControllerHome.php
+++ classes/controller/ControllerHome.php
@@ -5,12 +5,12 @@ namespace controller;
 class ControllerHome extends Controller{
 
     public function view(): void{
-        $this->render("test2.php", [
-            "test" => $this->get("test3.php")
+        $this->render("test2", [
+            "test" => $this->get("test3")
         ]);
     }
 
     public function add(): void{
-        $this->render("test.php", []);
+        $this->render("test", []);
     }
 }
