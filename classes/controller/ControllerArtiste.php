919f63874f486e444b0044e0821671a671d89a95
Kevin-LB <le.breton.kevin@icloud.com>
2024-01-30 14:21:39 +0100

    update

--- classes/controller/ControllerArtiste.php
+++ classes/controller/ControllerArtiste.php
@@ -4,13 +4,16 @@
 
 namespace controller;
 
+use models\db\AlbumDB;
+
 class ControllerArtiste extends Controller{
 
     public function view(){
+        $albums = AlbumDB::getAlbums();
         $this->render("base", [
             "header" => $this->get("element/header"),
             "menu" => $this->get("element/menu"),
-            "content" => $this->get("artiste")
+            "content" => $this->get("artiste", )
         ]);
     }
 }
--- view/artiste.php
+++ view/artiste.php
@@ -48,8 +48,85 @@
         </ul>
     </nav>
 
-
     <section>
+        <div class="musiques">
+            <div class="derniere-sortie">
+                <h2>
+                    Dernières sorties
+                </h2>
+                <div>
+                    <div class="element">
+                        <div>
+                            <img class="pochette" src="img/pochette.png" alt="">
+                        </div>
+                        <div class="text">
+                            <div class="title">
+                                <p>Titre</p>
+                                <div class="title-info">
+                                    <img class="icon" src="img/horloge.jpg" alt="">
+                                    <p>43:20</p>
+                                </div>
+                            </div>
+                            <p>Mis a jour le 27/01/2023</p>
+                        </div>
+                    </div>
+                    <div class="element">
+                        <div>
+                            <img class="pochette" src="img/pochette.png" alt="">
+                        </div>
+                        <div class="text">
+                            <div class="title">
+                                <p>Titre</p>
+                                <div class="title-info">
+                                    <img class="icon" src="img/horloge.jpg" alt="">
+                                    <p>43:20</p>
+                                </div>
+                            </div>
+                            <p>Mis a jour le 27/01/2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="top-titres">
+                <h2>
+                    Top Titres 
+                </h2>
+                <ul id="list-titles">
+                    <li>
+                        <p>
+                            j
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            j
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            j
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            j
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            j
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            j
+                        </p>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
 
     </section>
 </div>
