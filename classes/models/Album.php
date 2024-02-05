8791f24a8701ecfea4ade9f8356ca076a3e39d18
QuentinDroucheau <quentin.droucheau@etu.univ-orleans.fr>
2024-02-05 16:51:15 +0100

    modification render des albums

--- classes/models/Album.php
+++ classes/models/Album.php
@@ -3,6 +3,7 @@
 namespace models;
 
 use models\db\CollectionMusicale;
+use view\Template;
 
 class Album extends CollectionMusicale {
 
@@ -27,23 +28,14 @@ class Album extends CollectionMusicale {
         return $this->musiques;
     }
 
-    public function render(): string {
-        $output = '<div class="main-album">';
-        $output .= '<img class="img-album" src="' . $this->getImage() . '" alt="">';
-        $output .= '<section class="infos-card">';
-        $output .= '<div class="top-infos-card">';
-        $output .= '<h3>' . $this->getTitre() . '</h3>';
-        $output .= '<p>' . (count($this->getMusiques()) == 1 ? 'Single' : count($this->getMusiques()) . ' Titres') . '</p>';
-        $output .= '</div>';
-        $output .= '<div class="bottom-infos-card">';
-        $output .= '<div class="bottom-infos-card-artist">';
-        $output .= '<img src="../img/icone_artist.svg" alt="icone de l\'artiste"/>';
-        $output .= '<p>' . $this->getAuteur()->getNom() . '</p>';
-        $output .= '</div>';
-        $output .= '<p>' . $this->getAnneeAlbum()->format('d/m/Y') . '</p>';
-        $output .= '</div>';
-        $output .= '</section>';
-        $output .= '</div>';
-        return $output;
+    public function render(): string{
+        return Template::get("element/album", [
+            "image" => $this->getImage(),
+            "id" => $this->getId(),
+            "titre" => $this->getTitre(),
+            "musiques" => $this->getMusiques(),
+            "auteurNom" => $this->getAuteur()->getNom(),
+            "anneeAlbum" => $this->getAnneeAlbum()->format("d/m/Y")
+        ]);
     }
 }
---
+++ view/element/album.php
@@ -0,0 +1,32 @@
+<div class="main-album">
+
+    <img class="img-album" 
+        src=<?php echo $image;?> 
+        alt="" 
+        onclick="window.location.href=album?id=<?php echo $id;?>">
+
+    <section class="infos-card">
+        <div class="top-infos-card">
+            <h3><?php echo $titre;?></h3>
+            <p>
+                <?php echo (count($musiques) == 1 ? 'Single' : count($musiques))." Titres";?> 
+            </p>
+        </div>
+            <div class="bottom-infos-card">
+                <div class="bottom-infos-card-artist">
+                    <img src="../img/icone_artist.svg" alt="icone de l\'artiste"/>
+                    <p>
+                        <?php echo $auteurNom;?>
+                    </p>
+                </div>
+                <p>
+                    <?php echo $anneeAlbum?>
+                </p>
+            </div>
+    </section>
+</div>
+<script>
+    function redirect(id){
+        window.location.href="album?id="+id;
+    }
+</script>
