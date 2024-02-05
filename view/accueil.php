538262974f8e818a6cdce59a9198e5b4b3ef4fb1
araviart <alexandre.raviart@etu.univ-orleans.fr>
2024-02-05 15:51:11 +0100

    début d'implémentation swiper

--- view/accueil.php
+++ view/accueil.php
@@ -1,12 +1,23 @@
-<link rel="stylesheet" href = "styles/accueil.css">
+<script src="js/swiper-bundle.min.js" defer></script>
+<script src="js/slideAccueil.js" defer></script>
+
+<link rel="stylesheet" href="styles/accueil.css">
+<link rel="stylesheet" href="styles/swiper.css">
+
+
 <?php foreach ($albumsByCategory as $category => $lesAlbums) : ?>
     <section class="section-categorie-album">
-        <h2><?= ucfirst($category) ?></h2>
-        <button class="btn-voir-plus">plus</button>
-        <div class="albums">
-            <?php foreach ($lesAlbums as $album) : ?>
-                <?= $album->render() ?>
-            <?php endforeach ?>
+        <h2><?= $category ?></h2>
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <?php foreach ($lesAlbums as $album) : ?>
+                    <div class="swiper-slide">
+                        <?= $album->render() ?>
+                    </div> <!-- ferme le swiper-slide -->
+                <?php endforeach ?>
+            </div> <!-- ferme le wrapper -->
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
         </div>
     </section>
 <?php endforeach ?>
